540150ce3786f275ef4108fdcb5a99c874303671
Alberto de Olivira <betodeoliveira@outlook.com>
2024-03-05 16:10:43 -0300

    b1.0

--- src/index.ts
+++ src/index.ts
@@ -1,139 +1,51 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { TextPlugin } from "gsap/TextPlugin";
+// import { TextPlugin } from "gsap/TextPlugin";
 import SplitType from "split-type";
 
-gsap.registerPlugin(ScrollTrigger, TextPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
-let animationGroups = document.querySelectorAll("[anim-group]");
+function getAttributeAsFloat(element: Element, attribute: string, defaultValue: number = 0): number {
+    return parseFloat(element.getAttribute(attribute) as string) || defaultValue;
+}
 
-animationGroups.forEach((group, groupIndex) => {
-    let timeline = gsap.timeline();
-    let groupElements = group.querySelectorAll("[anim-group-el]");
-    let start = group.getAttribute("start") || "top 80%";
-    let end = group.getAttribute("end") || "bottom 20%";
-    let scrub;
-    if (group.hasAttribute("scrub")) {
-        if (group.getAttribute("scrub") === "true") { scrub = true; }
-        else {
-            scrub = parseFloat(group.getAttribute("scrub") as string);
-        }
-    }
-    groupElements.forEach((el, elIndex) => {
-        // Check if the element has an id applied and if not, apply one
-        if (!el.hasAttribute("id")) { el.setAttribute("id", `group-${groupIndex}-el-${elIndex}`); }
-        // Get default values from the settings element
-        let animType = el.getAttribute("anim-group-el");
-        let settingsEl = document.querySelector(`[anim-settings="${animType}"]`)!;
-        let duration = parseFloat(settingsEl.getAttribute("default-duration") as string) || 1;
-        let ease = settingsEl.getAttribute("default-ease") as string || "expo.out";
-        let posVector = ["0rem", "0rem"];
-        posVector[0] = settingsEl.getAttribute("default-pos-x") as string;
-        posVector[1] = settingsEl.getAttribute("default-pos-y") as string;
-        let rotVector = [0, 0];
-        rotVector[0] = parseFloat(settingsEl.getAttribute("default-rot-x") as string) || 90;
-        rotVector[1] = parseFloat(settingsEl.getAttribute("default-rot-y") as string) || 90;
-        let scale = settingsEl.getAttribute("default-scale") as string;
-        // Get value from the element if it exists, otherwise use the default
-        let stagger = parseFloat(el.getAttribute("stagger") as string) || 0;
-        let direction = (el.getAttribute("direction") as string) || "up";
-        let delay = parseFloat(el.getAttribute('delay') as string) || 0;
-        // Values that can be overriden by the element
-        let opacity = parseFloat(el.getAttribute('opacity') as string) || 0;
-        if (el.hasAttribute("duration")) { duration = parseFloat(el.getAttribute("duration") as string); }
-        if (el.hasAttribute("ease")) { ease = el.getAttribute("ease") as string; }
-        // Get the element to animate
-        let elToAnimate = getElementoToAnimate(el);
+function getAttributeAsString(element: Element, attribute: string, defaultValue: string = ''): string {
+    return element.getAttribute(attribute) as string || defaultValue;
+}
 
-        switch (animType) {
-            // Fades
-            case "fade-in":
-                timeline.from(elToAnimate, { opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            case "fade-out":
-                timeline.to(elToAnimate, { opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            // Grows
-            case "grow-in":
-                timeline.from(elToAnimate, { scale: scale, opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            case "grow-out":
-                timeline.to(elToAnimate, { scale: scale, opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            // Shrinks
-            case "shrink-in":
-                timeline.from(elToAnimate, { scale: scale, opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            case "shrink-out":
-                timeline.to(elToAnimate, { scale: scale, opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            // Slides
-            case "slide-in":
-                posVector = updatePosVector(direction, posVector);
-                timeline.from(elToAnimate, { x: posVector[0], y: posVector[1], opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            case "slide-out":
-                posVector = updatePosVector(direction, posVector);
-                timeline.to(elToAnimate, { x: posVector[0], y: posVector[1], opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            // Flips
-            case "flip-in":
-                rotVector = updateRotVector(direction, rotVector);
-                timeline.from(elToAnimate, { rotationX: rotVector[0], rotationY: rotVector[1], opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            case "flip-out":
-                rotVector = updateRotVector(direction, rotVector);
-                timeline.to(elToAnimate, { rotationX: rotVector[0], rotationY: rotVector[1], opacity: opacity, duration: duration, ease: ease, delay: delay, stagger: stagger }, 0);
-                break;
-            // Text Fades
-            case "text-fade-in":
-                timeline.from(elToAnimate, { opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            case "text-fade-out":
-                timeline.to(elToAnimate, { opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            // Text Grows
-            case "text-grow-in":
-                timeline.from(elToAnimate, { scale: scale, opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            case "text-grow-out":
-                timeline.to(elToAnimate, { scale: scale, opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            // Text Shrinks
-            case "text-shrink-in":
-                timeline.from(elToAnimate, { scale: scale, opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            case "text-shrink-out":
-                timeline.to(elToAnimate, { scale: scale, opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            // Text Slides
-            case "text-slide-in":
-                posVector = updatePosVector(direction, posVector);
-                timeline.from(elToAnimate, { x: posVector[0], y: posVector[1], opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            case "text-slide-out":
-                posVector = updatePosVector(direction, posVector);
-                timeline.to(elToAnimate, { x: posVector[0], y: posVector[1], opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            // Text Flips
-            case "text-flip-in":
-                rotVector = updateRotVector(direction, rotVector);
-                timeline.from(elToAnimate, { rotationX: rotVector[0], rotationY: rotVector[1], opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            case "text-flip-out":
-                rotVector = updateRotVector(direction, rotVector);
-                timeline.to(elToAnimate, { rotationX: rotVector[0], rotationY: rotVector[1], opacity: opacity, duration: duration, stagger: stagger, ease: ease, delay: delay }, 0);
-                break;
-            // case "text-replace":
-            //     let defaultText = settingsEl.getAttribute("default-text") as string;
-            //     let newText = el.getAttribute("new-text") as string || defaultText;
-            //     timeline.to(el, { text: newText, ease: ease, delay: delay }, 0);
-            //     break;
-        }
+function createAnimationConfig(el: Element, settingsEl: Element) {
+    // Get defaults for values
+    let x = getAttributeAsString(settingsEl, "default-pos-x", "0rem");
+    let y = getAttributeAsString(settingsEl, "default-pos-y", "0rem");
+    let rotationX = getAttributeAsFloat(settingsEl, "default-rot-x", 90);
+    let rotationY = getAttributeAsFloat(settingsEl, "default-rot-y", 90);
+    let scale = getAttributeAsString(settingsEl, "default-scale");
+    // Get defaults for timeline values
+    let duration = getAttributeAsFloat(settingsEl, "default-duration", 1);
+    let ease = getAttributeAsString(settingsEl, "default-ease", "expo.out");
+    let stagger = getAttributeAsFloat(settingsEl, "default-stagger", 0.1);
+    let direction = getAttributeAsString(settingsEl, "default-direction", "up");
+    // Check if element has values and if not, use the defaults
+    let delay = getAttributeAsFloat(el, 'delay');
+    let opacity = getAttributeAsFloat(el, 'opacity');
+    if (el.hasAttribute("duration")) { duration = getAttributeAsFloat(el, "duration"); }
+    if (el.hasAttribute("ease")) { ease = getAttributeAsString(el, "ease"); }
+    if (el.hasAttribute("stagger")) { stagger = getAttributeAsFloat(el, "stagger"); }
+    if (el.hasAttribute("direction")) { direction = getAttributeAsString(el, "direction"); }
+    if (el.hasAttribute("pos-x")) { x = getAttributeAsString(el, "pos-x"); }
+    if (el.hasAttribute("pos-y")) { y = getAttributeAsString(el, "pos-y"); }
+    if (el.hasAttribute("rot-x")) { rotationX = getAttributeAsFloat(el, "rot-x"); }
+    if (el.hasAttribute("rot-y")) { rotationY = getAttributeAsFloat(el, "rot-y"); }
+    if (el.hasAttribute("scale")) { scale = getAttributeAsString(el, "scale"); }
+    // Update the position vector
+    x = updatePosVector(direction, x, y).posX;
+    y = updatePosVector(direction, x, y).posY;
+    // Update the rotation vector
+    rotationX = updateRotVector(direction, rotationX, rotationY).rotX;
+    rotationY = updateRotVector(direction, rotationX, rotationY).rotY;
 
-        createScrollTrigger(group, start, end, scrub, timeline);
-    });
-});
+    return { duration, ease, stagger, direction, delay, opacity, x, y, rotationX, rotationY, scale };
+}
 
 function getElementoToAnimate(element) {
     let elToAnimate;
@@ -164,22 +76,60 @@ function getElementoToAnimate(element) {
     }
 }
 
-function updatePosVector(direction, posVector: Array<string>) {
-    if (direction === "up" || direction === "down") { posVector[0] = "0rem"; }
-    if (direction === "left" || direction === "right") { posVector[1] = "0rem"; }
-    if (direction === "right") { posVector[0] = "-" + posVector[0]; }
-    if (direction === "down") { posVector[1] = "-" + posVector[1]; }
-    return posVector;
+function updatePosVector(direction, x, y) {
+    if (direction === "up" || direction === "down") { x = "0rem"; }
+    if (direction === "left" || direction === "right") { y = "0rem"; }
+    if (direction === "right") { x = "-" + x; }
+    if (direction === "down") { y = "-" + y; }
+    return { posX: x, posY: y };
 }
 
-// A function to update Position and Rotation that receives a param that must be Move or Rotate
-function updateRotVector(direction, rotVector: Array<number>) {
-    if (direction === "left" || direction === "right") { rotVector[0] = 0; }
-    if (direction === "up" || direction === "down") { rotVector[1] = 0; }
-    if (direction === "down") { rotVector[0] = rotVector[0] * -1; }
-    if (direction === "left") { rotVector[1] = rotVector[1] * -1; }
-    return rotVector;
+function updateRotVector(direction, rotationX, rotationY) {
+    if (direction === "left" || direction === "right") { rotationX = 0; }
+    if (direction === "up" || direction === "down") { rotationY = 0; }
+    if (direction === "down") { rotationX = rotationX * -1; }
+    if (direction === "left") { rotationY = rotationY * -1; }
+    return { rotX: rotationX, rotY: rotationY };
 }
+
+function createAnimationFunction(properties: string[], method: 'from' | 'to') {
+    return (timeline: gsap.core.Timeline, elToAnimate: Element, config: any) => {
+        const animationProperties: any = {};
+        properties.forEach(property => {
+            if (config[property] !== undefined) {
+                animationProperties[property] = config[property];
+            }
+        });
+        animationProperties.duration = config.duration;
+        animationProperties.ease = config.ease;
+        animationProperties.delay = config.delay;
+        animationProperties.stagger = config.stagger;
+        timeline[method](elToAnimate, animationProperties, 0);
+    };
+}
+
+const animationConfig = {
+    'fade-in': createAnimationFunction(['opacity'], 'from'),
+    'fade-out': createAnimationFunction(['opacity'], 'to'),
+    'grow-in': createAnimationFunction(['scale', 'opacity'], 'from'),
+    'grow-out': createAnimationFunction(['scale', 'opacity'], 'to'),
+    'shrink-in': createAnimationFunction(['scale', 'opacity'], 'from'),
+    'shrink-out': createAnimationFunction(['scale', 'opacity'], 'to'),
+    'slide-in': createAnimationFunction(['x', 'y', 'opacity'], 'from'),
+    'slide-out': createAnimationFunction(['x', 'y', 'opacity'], 'to'),
+    'flip-in': createAnimationFunction(['rotationX', 'rotationY', 'opacity'], 'from'),
+    'flip-out': createAnimationFunction(['rotationX', 'rotationY', 'opacity'], 'to'),
+    'text-fade-in': createAnimationFunction(['opacity'], 'from'),
+    'text-fade-out': createAnimationFunction(['opacity'], 'to'),
+    'text-grow-in': createAnimationFunction(['scale', 'opacity'], 'from'),
+    'text-grow-out': createAnimationFunction(['scale', 'opacity'], 'to'),
+    'text-shrink-in': createAnimationFunction(['scale', 'opacity'], 'from'),
+    'text-shrink-out': createAnimationFunction(['scale', 'opacity'], 'to'),
+    'text-slide-in': createAnimationFunction(['x', 'y', 'opacity'], 'from'),
+    'text-slide-out': createAnimationFunction(['x', 'y', 'opacity'], 'to'),
+    'text-flip-in': createAnimationFunction(['rotationX', 'rotationY', 'opacity'], 'from'),
+    'text-flip-out': createAnimationFunction(['rotationX', 'rotationY', 'opacity'], 'to'),
+};
 
 function createScrollTrigger(triggerElement, start, end, scrub, timeline) {
 
@@ -195,3 +145,36 @@ function createScrollTrigger(triggerElement, start, end, scrub, timeline) {
 
     tl.add(timeline);
 }
+
+let animationGroups = document.querySelectorAll("[anim-group]");
+
+animationGroups.forEach((group, groupIndex) => {
+    let timeline = gsap.timeline();
+    let groupElements = group.querySelectorAll("[anim-group-el]");
+    let start = group.getAttribute("start") || "top 80%";
+    let end = group.getAttribute("end") || "bottom 20%";
+    let scrub;
+    if (group.hasAttribute("scrub")) {
+        if (group.getAttribute("scrub") === "true") { scrub = true; }
+        else {
+            scrub = parseFloat(group.getAttribute("scrub") as string);
+        }
+    }
+    groupElements.forEach((el, elIndex) => {
+        // Check if the element has an id applied and if not, apply one
+        if (!el.hasAttribute("id")) { el.setAttribute("id", `group-${groupIndex}-el-${elIndex}`); }
+        // Get default values from the settings element
+        let animType = el.getAttribute("anim-group-el")!;
+        let settingsEl = document.querySelector(`[anim-settings="${animType}"]`)!;
+        let config = createAnimationConfig(el, settingsEl);
+        let elToAnimate = getElementoToAnimate(el);
+        if (animationConfig[animType]) {
+            animationConfig[animType](timeline, elToAnimate, config);
+        }
+        createScrollTrigger(group, start, end, scrub, timeline);
+    });
+
+    // Reset the group opacity back to 1
+    const groupElement = group as HTMLElement;
+    groupElement.style.opacity = "1";
+});
